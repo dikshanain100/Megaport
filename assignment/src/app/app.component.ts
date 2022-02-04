81a5df4c1df0b1cc40adb31d0be1a92840f43feb
dikshanain100 <dikshanain100@gmail.com>
2022-02-04 18:04:59 +0530

    diff routes added on parent module

--- assignment/src/app/app.component.ts
+++ assignment/src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'assignment';
+export class AppComponent  {
+
+
+  constructor(private router: Router) { }
+
+
+  //  To navigate to different modules 
+  redirectModule(module) {
+    if (module == 1) {
+      this.router.navigate(['/bakery']);
+    }
+    else if (module == 2) {
+      this.router.navigate(['/address']);
+    }
+    else {
+      this.router.navigate(['/get']);
+    }
+  }
 }
